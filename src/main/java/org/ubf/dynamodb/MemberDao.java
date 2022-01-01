422d4d40af69d9c14ffcb96aff7ffa66d115a285
Ilya <ilya_us@yahoo.com>
2022-01-01 00:34:02 -0600

    Add fellowshipId search

--- src/main/java/org/ubf/dynamodb/MemberDao.java
+++ src/main/java/org/ubf/dynamodb/MemberDao.java
@@ -1,10 +1,13 @@
 package org.ubf.dynamodb;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import org.ubf.model.m_site.Member;
 import org.ubf.model.m_site.Members;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public class MemberDao extends BaseDynamoDb {
@@ -15,8 +18,32 @@ public class MemberDao extends BaseDynamoDb {
     return member;
   }
 
-  public Member get(String memberId) {
-    return mapper.load(Member.class, UUID.fromString(memberId));
+  public Members getMembersById(UUID memberId) {
+    DynamoDBQueryExpression<Member> queryExpression = new DynamoDBQueryExpression<Member>()
+        .withKeyConditionExpression("memberId = :memberId")
+        .withExpressionAttributeValues(Map.of(":memberId", new AttributeValue().withS(memberId.toString())));
+
+    return getMembers(queryExpression);
+  }
+
+  public Members getMembersByFellowshipId(UUID fellowshipId) {
+    Member member = new Member();
+    member.setFellowshipId(fellowshipId);
+
+    DynamoDBQueryExpression<Member> queryExpression = new DynamoDBQueryExpression<Member>()
+        .withHashKeyValues(member)
+        .withConsistentRead(false)
+        .withScanIndexForward(false);
+
+    return getMembers(queryExpression);
+  }
+
+  public Members getMembers(Map<String, String> filter) {
+    return new Members();
+  }
+
+  public Member get(UUID memberId) {
+    return mapper.load(Member.class, memberId);
   }
 
   public void delete(UUID id) {
@@ -30,5 +57,12 @@ public class MemberDao extends BaseDynamoDb {
     return mapper.scan(Member.class, new DynamoDBScanExpression());
   }
 
+  private Members getMembers(DynamoDBQueryExpression<Member> queryExpression) {
+    List<Member> result = mapper.query(Member.class, queryExpression);
 
+    Members members = new Members();
+    members.addAll(result);
+
+    return members;
+  }
 }
